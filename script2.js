d4f8fc3435e51c0c26ad0cd9ff0d34372fde7cf6
khaingZarkz <139622429+khaingZarkz@users.noreply.github.com>
2023-08-10 14:05:03 +0630

    Create script2.js
    script2.js

---
+++ script2.js
@@ -0,0 +1,13 @@
+function generateColor() {
+    let color = '#';
+    let digits = '0123456789ABCDEF';
+    for (let i = 0; i < 6; i++) {
+  
+      let randomDigit = Math.floor(Math.random() * 16);
+    
+      color += digits[randomDigit];
+    }
+    document.getElementById('color').innerHTML = color;
+    document.body.style.backgroundColor = color;
+  }
+  generateColor();
